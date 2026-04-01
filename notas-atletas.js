d79dcf3e371d25ae93f6796b5bc5889a0da9c946
Diego Manoel <diegomanoelmcosta@gmail.com>
2026-03-31 23:14:10 -0300

    Adicionado for para impressão do resultado

---
+++ notas-atletas.js
@@ -0,0 +1,30 @@
+// Matriz de objetos fornecido pelo projeto de certificação.
+let atletas = [
+    {
+        nome: "Cesar Abascal",
+        notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+        nome: "Fernando Puntel",
+        notas: [8, 10, 10, 7, 9.33]
+    },
+    {
+        nome: "Daiane Jelinsky",
+        notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+        nome: "Bruno Castro",
+        notas: [10, 10, 10, 9, 9.5]
+    }
+];
+
+// percorre atletas
+for (let i = 0; i < atletas.length; i++) {
+    let atleta = atletas[i];
+
+    let media = calculaMedia(atleta.notas);
+
+    console.log(`Atleta: ${atleta.nome}`);
+    console.log(`Notas Obtidas: ${atleta.notas}`);
+    console.log(`Média Válida: ${media}\n`);
+};
